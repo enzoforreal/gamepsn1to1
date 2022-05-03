c58711cbdb5c40439853da6b2277317b795f7f00
renaud <enzoforreal@gmail.com>
2022-05-03 06:28:36 +0200

    link img src for image game

--- views/Utilisateur/partie.view.php
+++ views/Utilisateur/partie.view.php
@@ -85,7 +85,8 @@
                   <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
                         <div class="col">
                               <div class="card">
-                                    <img src="https://picsum.photos/350" class="card-img-top" alt="...">
+                                    <img src="../../public/Assets/images/fortnitepc.png" class="card-img-top"
+                                          alt="image game">
                                     <div class="card-body">
 
                                           <h5 class="card-title fw-bold">Jeux: Fortnite</h5>
