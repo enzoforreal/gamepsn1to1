39887e4ead5cb88fe6a76768e3bbbd805b1870f7
renaud <enzoforreal@gmail.com>
2022-05-08 02:10:46 +0200

    updated front  captcha in login and register

--- views/Visiteur/login.view.php
+++ views/Visiteur/login.view.php
@@ -5,23 +5,27 @@
             <div class="d-flex flex-column align-items-center">
 
                   <div class="m-5">
-                        <img class="mx-auto d-block mb-3" src="../../public/Assets/images/logo.svg" alt="Logo" width="200px" style=" object-fit: cover;">
+                        <img class="mx-auto d-block mb-3" src="../../public/Assets/images/logo.svg" alt="Logo"
+                              width="100px" style=" object-fit: cover;">
                         <h1 class="text-center fw-bold">Login</h1>
-                        <p class="text-center">Connect to your account</p>
+
                   </div>
 
-                  <form method="POST" action="<?= URL ?>validation_login" class="d-flew flew-column" style="width: 400px">
+                  <form method="POST" action="<?= URL ?>validation_login" class="d-flew flew-column"
+                        style="width: 400px">
                         <div class="mb-3">
                               <label for="login">Lastname</label>
-                              <input type="text" name="login" id="login" class="form-control" placeholder="Your lastname" require="required">
+                              <input type="text" name="login" id="login" class="form-control"
+                                    placeholder="Your lastname" require="required">
                         </div>
 
                         <div class="mb-3">
                               <label for="password">Password</label>
-                              <input type="password" name="password" id="password" class="form-control" placeholder="Your password" require="required">
+                              <input type="password" name="password" id="password" class="form-control"
+                                    placeholder="Your password" require="required">
                         </div>
-                        <h6 class="title">Captcha puzzle</h6>
-                        <table width="225" border="0" cellspacing="0" cellpadding="0" height="225">
+
+                        <table class="captcha" width="225" border="0" cellspacing="0" cellpadding="0" height="225">
                               <tr>
                                     <td id="image1" name="image2" class="i6" onclick="clickImage('image1')"></td>
                                     <td id="image2" name="image3" class="i2" onclick="clickImage('image2')"></td>
@@ -32,7 +36,9 @@
                               </tr>
                         </table>
 
-                        <button disabled="True" id="button" type="submit" class="btn btn-primary w-100">Connect</button>
+                        <button style="position:absolute;top: 800px; right: 860px;" disabled="True" id="button"
+                              type="submit" class="btn btn-success btn-sm">Connect to your
+                              account</button>
                   </form>
 
             </div>
