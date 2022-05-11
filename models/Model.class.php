5dcc1a922278b069b3c1637b7cf27c8db301020e
renaud <enzoforreal@gmail.com>
2022-05-11 14:53:37 +0200

    name database changed

--- models/Model.class.php
+++ models/Model.class.php
@@ -4,7 +4,7 @@ abstract class Model{
     private static $pdo;
 
     private static function setBdd(){
-        self::$pdo = new PDO("mysql:host=localhost:3307;dbname=game1to1bis;charset=utf8", "root", "root"); 
+        self::$pdo = new PDO("mysql:host=localhost:3307;dbname=game1to1;charset=utf8", "root", "root"); 
         //self::$pdo = new PDO("mysql:host=localhost:3306;dbname=game1to1;charset=utf8", "root", "root"); config nathan
         //self::$pdo = new PDO("mysql:host=localhost;dbname=game1to1;charset=utf8", "root", "Peyton242"); config  serveur 
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
