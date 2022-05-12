ecb9712c701ee5d9f9e06065f4530e55c08b6d3b
renaud <enzoforreal@gmail.com>
2022-05-12 19:28:36 +0200

    room party updated

--- views/Utilisateur/roomParty.view.php
+++ views/Utilisateur/roomParty.view.php
@@ -1,80 +1,89 @@
-
 <div class="container-fluid">
-    <div class="row">
+      <div class="row">
 
-        <aside class="col-12 col-md-3 custom-background-light border-end border-bottom p-0 d-flex flex-column">
-            <p class="text-center m-3">CHAT PRIVEE</p>
+            <aside class="col-12 col-md-3 custom-background-light border-end border-bottom p-0 d-flex flex-column">
+                  <p class="text-center m-3">CHAT PRIVEE</p>
 
-            <!--Insérer les nouveaux messages du chat ici-->
-            <div class="list-group list-group-flush scrollarea">
-                
-                <!--Renvoie vers le profil de l'utilisateur qui à posté ce message-->
-                <a href="" class="list-group-item">
-                    <div class="ms-2 me-auto">
-                        <div class="d-flex w-100">
-                            <img src="https://picsum.photos/200" class="me-2" alt="" style="height: 20px">
-                            <h5 class="fw-bold">Marie</h5>
+                  <!--Insérer les nouveaux messages du chat ici-->
+                  <div class="list-group list-group-flush scrollarea">
+
+                        <!--Renvoie vers le profil de l'utilisateur qui à posté ce message-->
+                        <a href="" class="list-group-item">
+                              <div class="ms-2 me-auto">
+                                    <div class="d-flex w-100">
+                                          <img src="https://picsum.photos/200" class="me-2" alt="" style="height: 20px">
+                                          <h5 class="fw-bold">Marie</h5>
+                                    </div>
+                                    <small class="ms-auto">15 seconds ago</small>
+                                    <div>Je veux être dans l'équipe rouge</div>
+                              </div>
+                        </a>
+
+                  </div>
+            </aside>
+
+            <main class="col-12 col-md-9 custom-content">
+                  <div class="row">
+
+                        <!--Couverture du jeu-->
+                        <div class="col-12 col-xl-6 text-center mb-5">
+                              <img src="../../public/Assets/images/img_room.png" alt="" class="img-fluid w-100"
+                                    style="max-height: 400px; object-fit: cover;">
                         </div>
-                        <small class="ms-auto">15 seconds ago</small>
-                        <div>Je veux être dans l'équipe rouge</div>
-                    </div>
-                </a>
 
-            </div>
-        </aside>
+                        <!--Joueurs présent dans la salle-->
+                        <div class="list-group list-group-flush col-12 col-xl-6 mb-5">
+                              <div class="list-group-item">
+                                    <div class="">
+                                          <div>
+                                                <h7 class="fw-bold">Partie crée par : <?= $utilisateur['login'] ?>
+                                                </h7>
+                                          </div>
+                                          <div class="d-flex w-100">
+                                                <img src="https://picsum.photos/200" class="me-2" alt=""
+                                                      style="height: 20px">
 
-        <main class="col-12 col-md-9 custom-content">
-            <div class="row">
+                                                <h5 class="fw-bold">Login: <?= $utilisateur['login'] ?></h5>
+                                                <small class="ms-auto text-success">Ready</small>
+                                          </div>
+                                          <div>Pseudo plateforme: Dark_Sasukexx63</div>
+                                          <div>Mise: 20$</div>
+                                    </div>
+                              </div>
 
-                <!--Couverture du jeu-->
-                <div class="col-12 col-xl-6 text-center mb-5">
-                    <img src="https://picsum.photos/400" alt="" class="img-fluid w-100" style="max-height: 400px; object-fit: cover;">
-                </div>
+                              <div class="list-group-item">
+                                    <div class="">
+                                          <div class="d-flex w-100">
+                                                <img src="https://picsum.photos/200" class="me-2" alt=""
+                                                      style="height: 20px">
 
-                <!--Joueurs présent dans la salle-->
-                <div class="list-group list-group-flush col-12 col-xl-6 mb-5">
-                    <div class="list-group-item">
-                        <div class="">
-                            <div class="d-flex w-100">
-                                <img src="https://picsum.photos/200" class="me-2" alt="" style="height: 20px">
-                                <h5 class="fw-bold">Pseudo site: Marie</h5>
-                                <small class="ms-auto text-success">Ready</small>
-                            </div>
-                            <div>Pseudo plateforme: Dark_Sasukexx63</div>
-                            <div>Mise: 20$</div>
+                                                <h5 class="fw-bold">Login: user 2</h5>
+                                                <small class="ms-auto text-danger">Not Ready</small>
+                                          </div>
+                                          <div>Pseudo plateforme: Dark_Sasukexx63</div>
+                                          <div>Mise: 20$</div>
+                                    </div>
+                              </div>
+
+                              <div class="col-12 mb-5">
+                                    <h3 class="d-block mb-2 fw-bold">Informations</h3>
+                                    <hr class="w-100">
+                                    <div class="row">
+                                          <h5 class="mb-2">Plateforme: PC</h5>
+                                          <h5 class="mb-2">Statut jeu: privée</h5>
+                                          <h5 class="mb-2">Identifiant partie: #47013</h5>
+                                    </div>
+                              </div>
+
+                              <div class="col-12 row justify-content-md-between">
+                                    <a href="<?= URL; ?>partie"
+                                          class="col-12 col-md-5 btn btn-outline-danger mb-3">Quit</a>
+                                    <button class="col-12 col-md-5 btn btn-danger mb-3">Ready</button>
+                              </div>
+
+
                         </div>
-                    </div>
+            </main>
 
-                    <div class="list-group-item">
-                        <div class="">
-                            <div class="d-flex w-100">
-                                <img src="https://picsum.photos/200" class="me-2" alt="" style="height: 20px">
-                                <h5 class="fw-bold">Pseudo site: Marie</h5>
-                                <small class="ms-auto text-danger">Not Ready</small>
-                            </div>
-                            <div>Pseudo plateforme: Dark_Sasukexx63</div>
-                            <div>Mise: 20$</div>
-                        </div>
-                    </div>
-
-                <div class="col-12 mb-5">
-                    <h3 class="d-block mb-2 fw-bold">Informations</h3>
-                    <hr class="w-100">
-                    <div class="row">
-                        <h5 class="mb-2">Plateforme: PC</h5>
-                        <h5 class="mb-2">Statut partie: privée (entre ami)</h5>
-                        <h5 class="mb-2">Identifiant partie: #47013</h5>
-                    </div>
-                </div>
-
-                <div class="col-12 row justify-content-md-between">
-                    <button class="col-12 col-md-5 btn btn-outline-danger mb-3">Quit</button>
-                    <button class="col-12 col-md-5 btn btn-danger mb-3">Ready</button>
-                </div>
-
-
-            </div>
-        </main>
-
-    </div>
+      </div>
 </div>
