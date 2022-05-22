8ca70d3a340f42da452896b32c28bc958741df88
renaud <enzoforreal@gmail.com>
2022-05-22 18:04:13 +0200

    type="text/css" added about type mime

--- website/views/common/template.php
+++ website/views/common/template.php
@@ -11,10 +11,10 @@
 
 
 
-      <link href="<?= URL ?>public/CSS/component.css" rel="stylesheet" />
+      <link href="<?= URL ?>public/CSS/component.css" rel="stylesheet" type="text/css" />
       <?php if(!empty($page_css)) : ?>
       <?php foreach($page_css as $fichier_css) : ?>
-      <link href="<?= URL ?>public/CSS/<?= $fichier_css ?>" rel="stylesheet" />
+      <link href="<?= URL ?>public/CSS/<?= $fichier_css ?>" rel="stylesheet" type="text/css" />
       <?php endforeach; ?>
       <?php endif; ?>
 </head>
