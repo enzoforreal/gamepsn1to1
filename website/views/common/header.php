b1b880558cc1129763c811c7607a58bb78306f9d
renaud <enzoforreal@gmail.com>
2022-05-24 02:35:26 +0200

    update

--- website/views/common/header.php
+++ website/views/common/header.php
@@ -12,7 +12,7 @@
                   <ul class="nav col-12 col-lg-auto justify-content-center">
 
                         <li>
-                              <a class="nav-link text-light" aria-current="page" href="<?= URL; ?>accueil">Home</a>
+                              <a class="nav-link text-light" aria-current="page" href="<?= URL; ?>accueil">Home page</a>
                         </li>
 
                         <?php if(!Securite::estConnecte()) : ?>
