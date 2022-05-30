db449af1685a6767b52f3c93de8b4ac9b8bb2cce
renaud <enzoforreal@gmail.com>
2022-05-30 18:12:39 +0200

    Auto stash before merge of "renaud-DR" and "main"

--- website/public/JavaScript/partie.js
+++ website/public/JavaScript/partie.js
@@ -1,22 +1,22 @@
-var myRange = document.querySelector("#myRange");
-var myValue = document.querySelector("#myValue");
-var myUnits = "€";
-var off = myRange.offsetWidth / (parseInt(myRange.max) - parseInt(myRange.min));
-var px =
-  (myRange.valueAsNumber - parseInt(myRange.min)) * off -
-  myValue.offsetParent.offsetWidth / 2;
+// var myRange = document.querySelector("#myRange");
+// var myValue = document.querySelector("#myValue");
+// var myUnits = "€";
+// var off = myRange.offsetWidth / (parseInt(myRange.max) - parseInt(myRange.min));
+// var px =
+//   (myRange.valueAsNumber - parseInt(myRange.min)) * off -
+//   myValue.offsetParent.offsetWidth / 2;
 
-myValue.parentElement.style.left = px + "px";
-myValue.parentElement.style.top = myRange.offsetHeight + "px";
-myValue.innerHTML = myRange.value + " " + myUnits;
+// myValue.parentElement.style.left = px + "px";
+// myValue.parentElement.style.top = myRange.offsetHeight + "px";
+// myValue.innerHTML = myRange.value + " " + myUnits;
 
-myRange.oninput = function () {
-  let px =
-    (myRange.valueAsNumber - parseInt(myRange.min)) * off -
-    myValue.offsetWidth / 2;
-  myValue.innerHTML = myRange.value + " " + myUnits;
-  myValue.parentElement.style.left = px + "px";
-};
+// myRange.oninput = function () {
+//   let px =
+//     (myRange.valueAsNumber - parseInt(myRange.min)) * off -
+//     myValue.offsetWidth / 2;
+//   myValue.innerHTML = myRange.value + " " + myUnits;
+//   myValue.parentElement.style.left = px + "px";
+// };
 
 window.getPartyList = function getPartyList(e) {
   let url = document
